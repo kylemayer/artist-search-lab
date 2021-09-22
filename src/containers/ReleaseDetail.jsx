9395a12276b3ef6347986744fd62e1b8ce61b8f4
Elise Muellerleile <eliamue@live.com>
2021-09-22 14:00:05 -0500

    mild styling

--- src/containers/ReleaseDetail.jsx
+++ src/containers/ReleaseDetail.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import SongList from '../components/displays/SongList';
 import { getSongs } from '../services/fetchApi.js';
 import { useParams } from 'react-router-dom';
+import styles from '../components/styles/ReleaseDetail.css';
 
 const ReleaseDetail = () => {
   const { id, artist } = useParams();
@@ -17,9 +18,9 @@ const ReleaseDetail = () => {
   if (loading) return <h3>Loading...</h3>;
 
   return (
-    <>
+    <div className={styles.songlist}>
       <SongList songs={songs} artist={artist} />
-    </>
+    </div>
   );
 };
 
